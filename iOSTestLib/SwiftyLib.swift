a7637392685238911ce930d7d5dfad4be27b9dec
tanguyen <tanguyen@petsmart.com>
2023-03-02 16:41:08 -0800

    Added test cases

---
+++ iOSTestLib/SwiftyLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLib.swift
+//  iOSTestLib
+//
+//  Created by Duc Nguyen on 3/2/23.
+//
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
