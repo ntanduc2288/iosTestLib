b6b0bebd3dacde63f8d12124fe613b6249c520d1
tanguyen <tanguyen@petsmart.com>
2023-03-02 16:17:02 -0800

    Intitial project setup

---
+++ iOSTestLib/iOSTestLib.h
@@ -0,0 +1,18 @@
+//
+//  iOSTestLib.h
+//  iOSTestLib
+//
+//  Created by Duc Nguyen on 3/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSTestLib.
+FOUNDATION_EXPORT double iOSTestLibVersionNumber;
+
+//! Project version string for iOSTestLib.
+FOUNDATION_EXPORT const unsigned char iOSTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSTestLib/PublicHeader.h>
+
+
---
+++ iOSTestLibExamples/iOSTestLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestLibExamplesApp.swift
+//  iOSTestLibExamples
+//
+//  Created by Duc Nguyen on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
